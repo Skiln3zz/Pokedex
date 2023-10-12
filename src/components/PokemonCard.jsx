d18340c4f0a3bae06899d7d66ad9100272e36536
Skiln3zz <guigui.puh@gmail.com>
2023-10-12 09:44:07 +0200

    upquete2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,42 +1,19 @@
 import React from 'react';
 
-const PokemonCard = (props) => {
-  console.log(props);
+function PokemonCard(props) {
+  const { pokemon }  = props;
 
   return (
-    <div className="pokemon-card">
-      <figure>
-        {props.pokemon.imgSrc ? (
-          <img src={props.pokemon.imgSrc} alt={props.pokemon.name} />
-        ) : (
-          <p>???</p>
-        )}
-        <figcaption>{props.pokemon.name}</figcaption>
-      </figure>
-    </div>
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
   );
-};
+  
+}
 
-const App = () => {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      imgSrc: "mew.png",
-    },
-  ];
-
-  const pokemon = pokemonList[1]; 
-
-  return (
-    <div className="App">
-      <PokemonCard pokemon={pokemon} />
-    </div>
-  );
-};
-
-export default App;
+export default PokemonCard;
